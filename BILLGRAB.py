a72ee9a88b7a576c0c46d838480d58e911efc1c7
zykie651 <36174711+zykie651@users.noreply.github.com>
2023-04-04 10:06:16 -0500

    Update BILLGRAB.py

--- BILLGRAB.py
+++ BILLGRAB.py
@@ -1,52 +1,54 @@
 from bs4 import BeautifulSoup
 import requests
 
-def perfect_bill_grab(url):
+def PERFECTBILLLGRAB(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     spans = soup.find_all('span')
     start_flag = False
     end_flag = False
-    bills = []
-    for span in spans:
-        if not start_flag:
-            if span.text == 'PERFECTION OF BILLS':
-                start_flag = True
-        else:
-            if span.text == 'FINAL PASSAGE OF BILLS':
-                end_flag = True
-                break
-            else:
-                bills.append(span.text)
-    if not start_flag:
-        print('Text not found. "PERFECTION OF BILLS" not found in the document.')
-    elif not end_flag:
-        print('Text not found. "FINAL PASSAGE OF BILLS" not found in the document.')
-    else:
-        print('Bills extracted successfully from the document.')
-    return bills
 
-def final_bill_grab(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    spans = soup.find_all('span')
-    start_flag = False
-    end_flag = False
-    bills = []
-    for span in spans:
-        if not start_flag:
-            if span.text == 'FINAL PASSAGE OF BILLS':
-                start_flag = True
-        else:
-            if span.text == 'RESOLUTIONS':
-                end_flag = True
-                break
+    with open("bills.txt", "w", encoding="utf8") as file:
+        for span in spans:
+            if not start_flag:
+                if span.text == 'PERFECTION OF BILLS':
+                    start_flag = True
             else:
-                bills.append(span.text)
+                if span.text == 'FINAL PASSAGE OF BILLS':
+                    end_flag = True
+                    break
+                else:
+                    file.write(span.text + "\n") # write span text to file
+
     if not start_flag:
         print('Text not found. "FINAL PASSAGE OF BILLS" not found in the document.')
     elif not end_flag:
         print('Text not found. "RESOLUTIONS" not found in the document.')
     else:
-        print('Bills extracted successfully from the document.')
-    return bills
+        print('Spans written to perfectionbills.txt')
+
+def FINALBILLGRAB(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    spans = soup.find_all('span')
+    start_flag = False
+    end_flag = False
+
+    with open("bills.txt", "a", encoding="utf8") as file:
+        for span in spans:
+            if not start_flag:
+                if span.text == 'FINAL PASSAGE OF BILLS':
+                    start_flag = True
+            else:
+                if span.text == 'RESOLUTIONS':
+                    end_flag = True
+                    break
+                else:
+                    file.write(span.text + "\n") # write span text to file
+
+    if not start_flag:
+        print('Text not found. "FINAL PASSAGE OF BILLS" not found in the document.')
+    elif not end_flag:
+        print('Text not found. "RESOLUTIONS" not found in the document.')
+    else:
+        print('Spans written to finalbills.txt')
